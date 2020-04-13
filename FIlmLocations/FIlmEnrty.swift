922db46b4edced01937caa6432727178677affd6
Henry Calderon <theceintiant@gmail.com>
2020-04-13 15:57:49 -0700

    Step 6

---
+++ FIlmLocations/FIlmEnrty.swift
@@ -0,0 +1,16 @@
+//
+//  FIlmEnrty.swift
+//  FIlmLocations
+//
+//  Created by Henry Calderon on 4/13/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import Foundation
+
+struct FilmEntry {
+    var firstActor: String
+    var locations: String
+    var releaseYear: String
+    var title: String
+}
