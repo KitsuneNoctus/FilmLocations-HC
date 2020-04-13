d4ee6f78c8e5f061657a525f5dfaaf453a30bad2
Henry Calderon <theceintiant@gmail.com>
2020-04-13 15:46:48 -0700

    step 4

--- FIlmLocations/ViewController.swift
+++ FIlmLocations/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getDataFromFile("locations")
+    }
+    
+    //method name suggestion
+    func getDataFromFile(_ fileName:String){
+        let path = Bundle.main.path(forResource: fileName, ofType: ".json")
+        if let path = path {
+          let url = URL(fileURLWithPath: path)
+          print(url)
+            //STuff-----------------
+            let contents = try? Data(contentsOf: url)
+            do {
+              if let data = contents,
+              let jsonResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:Any]] {
+                print(jsonResult)
+              }
+            } catch {
+              print("Error deserializing JSON: \(error)")
+            }
+        }
+        
     }
 
 
